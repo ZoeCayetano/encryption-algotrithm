72ff920ec37977b6905d91b7688a4945dbc2bf12
Zoë Cayetano <zcayetan@asu.edu>
2016-06-07 23:19:14 -0700

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//************************************************************************************************************** 
+// FILE:        Main.cpp
+//
+// DESCRIPTION: Contains the main() function. Instantiates a Test object which tests the Encryptor and
+//              Decryptor classes.
+//
+// AUTHORS:     Zoe Cayetano            (zcayetan@asu.edu)
+// 
+// COURSE:      CSE100 Principles of Programming with C++, Spring 2016
+//**************************************************************************************************************
+#include "Test.hpp"
+
+int main()
+{
+    Test test;
+    test.run();
+    return 0;
+}
