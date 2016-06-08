ce96e72f2489d0adf8a6ad264fff9c87d0f03210
Zoë Cayetano <zcayetan@asu.edu>
2016-06-07 23:19:57 -0700

    Add test.hpp

---
+++ Test.hpp
@@ -0,0 +1,19 @@
+//************************************************************************************************************** 
+// FILE:        Test.hpp
+//
+// DESCRIPTION: Class declaration for a class named Test that tests the Encryptor and Decryptor classes.
+//
+// AUTHORS:     Zoe Cayetano            (zcayetan@asu.edu)
+//
+// COURSE:      CSE100 Principles of Programming with C++, Spring 2016
+//**************************************************************************************************************
+#ifndef TEST_HPP
+#define TEST_HPP
+
+class Test {
+public:
+    Test();
+    void run();
+};
+
+#endif
